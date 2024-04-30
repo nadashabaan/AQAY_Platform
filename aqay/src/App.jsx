488fd5ebdbb9960a0a4fd23375c66473f620594f
Nada Elamir <93342962+nadashabaan@users.noreply.github.com>
2024-04-30 21:33:55 +0300

    router config

--- aqay/src/App.jsx
+++ aqay/src/App.jsx
@@ -1,17 +1,29 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import SignUpForm from "./Pages/SignUp";
 
-function App() {
-  const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <SignUpForm />
-    </>
-  );
-}
+export default App;
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route path="*" element={<NotFoundPage />} />
+      <Route index element={<HomePage />} />
+      <Route path="/SignUp" element={<SignUpForm />} />
+      <Route path="/jobs/:id" element={<JobPage />} />
+    </Route>
+  )
+);
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
