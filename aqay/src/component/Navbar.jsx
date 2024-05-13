e2f1d3cfe7e061311c55ef2778620ed4513cacfb
RoaaElrawy <112595000+RoaaElrawy@users.noreply.github.com>
2024-05-13 14:46:16 +0300

    change border color to   search

--- aqay/src/component/Navbar.jsx
+++ aqay/src/component/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
                 placeholder="Search..."
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
-                className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-orange-100 placeholder-gray-500 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm "
+                className="block w-full pl-10 pr-3 py-2 border border-black rounded-md leading-5 bg-orange-100 placeholder-gray-500 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm "
               />
               <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                 <RiSearchLine className="h-5 w-5 text-gray-400" />{" "}
