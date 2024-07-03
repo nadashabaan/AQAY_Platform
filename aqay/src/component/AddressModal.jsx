27c44daa42fd0fea70d2ff338eea2aa32aac7dc0
RoaaElrawy <112595000+RoaaElrawy@users.noreply.github.com>
2024-07-03 08:23:49 +0300

    Add pop address

--- aqay/src/component/AddressModal.jsx
+++ aqay/src/component/AddressModal.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from "react";
-
 const AddressModal = ({ isOpen, onClose, onSubmit }) => {
   const [address, setAddress] = useState("");
-
+  const [promoCode, setPromoCode] = useState("");
   if (!isOpen) return null;
-
   const handleSubmit = () => {
     onSubmit(address);
     setAddress("");
+    onSubmit(promoCode);
+    setPromoCode("");
   };
-
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white p-6 rounded-md shadow-lg w-full max-w-md">
@@ -23,8 +22,16 @@ const AddressModal = ({ isOpen, onClose, onSubmit }) => {
             value={address}
             onChange={(e) => setAddress(e.target.value)}
           />
+          <label className="block text-gray-700">Promo-Code</label>
+          <input
+            type="text"
+            className="w-full p-2 text-black-500 border border-black-300 rounded-md"
+            placeholder=" Enter Promo-Code"
+            value={promoCode}
+            onChange={(e) => setPromoCode(e.target.value)}
+          />
         </div>
-        <p className="mb-4 text-red-500">
+        <p className="mb-4 text-orange-500">
           The only payment method available is Cash on Delivery.
         </p>
         <div className="flex justify-end space-x-2">
@@ -36,7 +43,7 @@ const AddressModal = ({ isOpen, onClose, onSubmit }) => {
           </button>
           <button
             onClick={handleSubmit}
-            className="bg-red-500 text-white px-4 py-2 rounded"
+            className="bg-orange-500 text-white px-4 py-2 rounded"
           >
             OK
           </button>
